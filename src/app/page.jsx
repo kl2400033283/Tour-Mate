26b0a07a5dad2b0b1be2a9b1c9534f202030825d
Thiruvengala Brahmani Mandru <thiruvengalabrahmanimandru@gmail.com>
2026-02-24 16:55:25 +0000

    i need backgroung image of hawa mahal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,7 +10,7 @@ import {
 
 export default function Page() {
   const imageUrl =
-    'https://images.unsplash.com/photo-1599661046227-b2484051336c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+    'https://images.unsplash.com/photo-1617540156434-635237894d35?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
   return (
     <div className="relative h-screen w-screen text-white">
